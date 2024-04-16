dd2ba407246529cd1ddfaf0fa2f77a703c455195
Danilo Dantas <danilodantas7002@gmail.com>
2024-04-16 13:08:28 -0300

    Moved some other old files.

--- src/modules/Terminal/old/older/old/libs/main/commands.py
+++
@@ -1,8 +0,0 @@
-def help_(lib, function=""):
-    """
-    Replies with a helpful message about the function.
-    """
-    try:
-        eval(f"help(lib.{function})")
-    except AttributeError:
-        print(f"Function <{function}> not found.")
